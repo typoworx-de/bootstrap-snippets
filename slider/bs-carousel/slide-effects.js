c31f698d7f4832ef7b996a2ae099008a0cca7acf
typoworx-de <info@typoworx.de>
2018-07-31 11:11:00 +0200

    Update slide-effects.js

--- slider/bs-carousel/slide-effects.js
+++ slider/bs-carousel/slide-effects.js
@@ -1,40 +1,39 @@
 (function($) {
    $(document).ready(function() {
-   
+
       /**
        * Simple slide effect for BS-Slider
        * using custom effect.in/.out callback
        */
       (function(slider, slideSpeed) {
-        var effects = {};
+         var effects = {};
 
-        effects.out = function(el, speed) {
-          $(el).css({
-            bottom: '-100%',
-            opacity: 0
-              })
+         effects.out = function(el, speed, callbackFn) {
+            $(el).css({
+               bottom: '-100%',
+               opacity: 0
+              }, speed);
+
+            if(callbackFn instanceof Function) $.proxy(callbackFn, el)();
           };
-        effects.in = function(el, speed, callbackFn) {
-          $(el).animate({
+         effects.in = function(el, speed, callbackFn) {
+            $(el).animate({
                  bottom: '0',
                  opacity: 1
               }, {
-            duration: speed,
-            complete: callbackFn
+               duration: speed,
+               complete: callbackFn
               })
           };
 
-        effects.out(
-          $('.ce-bodytext', slider),
-          slideSpeed,
-          function() {
-            effects.in(this, slideSpeed);
+         effects.out(
+            $('.ce-bodytext', slider),
+            slideSpeed,
+            function() {
+               effects.in(this, slideSpeed);
               }
-        );
-
-        //$('.ce-bodytext', slider).fadeOut(0).fadeIn('slow');
-
-        $(slider).on('slide.bs.carousel slid.bs.carousel', function (e) {
+         );
+         $(slider).on('slide.bs.carousel slid.bs.carousel', function (e) {
             if(e.type === 'slid') {
               effects.in($('.ce-bodytext', this), slideSpeed);
             } else {
